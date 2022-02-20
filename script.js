b79293861f8b0d1bbc8197a22b87e9d7c08985bf
Jeff LeBoeuf <jeff@jeff.how>
2022-02-20 09:25:00 -0500

    Google Analytics

--- script.js
+++ script.js
@@ -15,34 +15,22 @@ function validateSubscriberForm(e) {
 
   let subscriberForm = e.target;
   
-  if (subscriberForm['subject'].value!='') { // caught a bot
-    toggleModal();
-
-  } else {
-    // store honeypot
-    const honeypot = subscriberForm['subject']
-    // remove honeypot
-    subscriberForm['subject'].remove()
-    // convert formdata for fetch
-    const data = new URLSearchParams();
-    for (const pair of new FormData( e.target )) {
-        data.append(pair[0], pair[1]);
-    }
-
-    fetch(
-      e.target.action, {
-        method: 'POST',
-        mode: 'no-cors',
-        body: data
-      })
-      .then(result => {
-        // set up honeypot again
-        subscriberForm.append(honeypot);
-        subscriberForm.reset();
-        toggleModal();
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
+  const data = new URLSearchParams();
+  for (const pair of new FormData( subscriberForm )) {
+      data.append(pair[0], pair[1]);
   }
+
+  fetch(
+    e.target.action, {
+      method: 'POST',
+      mode: 'no-cors',
+      body: data
+    })
+    .then(result => {
+      subscriberForm.reset();
+      toggleModal();
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 }
